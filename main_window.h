3eb7c1c72494361e5fb8d8f27f84d3980c3496ff
marko.harjula <marko.harjula@c9481a18-dd67-11de-bf67-198b766644d4>
2009-11-30 14:52:37 +0000

    prework for tree data

--- main_window.h
+++ main_window.h
@@ -27,12 +27,15 @@ public:
 private:
 	Ui::MainWindow ui;
 	void build_zone(QString);
+	void load_nif_list(QString);
 	void write_height(QString);
 	void build_height(void);
 	MagickWand *out_img;
 	MagickWand *terrain_img;
 	MagickWand *offset_img;
-	
+	QList<QString> nif_filenames;
+	QList<QString> nif_name;
+
 private slots:	    
 	void open_filedialog_daoc(void);
 	void open_filedialog_build(void);
