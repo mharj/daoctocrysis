9f694863832a1061ccf6c5962ffc23e8b2f52adb
marko.harjula <marko.harjula@c9481a18-dd67-11de-bf67-198b766644d4>
2009-12-20 13:15:19 +0000

    veg export

--- main_window.h
+++ main_window.h
@@ -11,6 +11,7 @@
 #include <QMessageBox>
 #include <QtGlobal>
 #include <wand/MagickWand.h>
+#include <math.h>
 
 using namespace std;
 
@@ -29,8 +30,12 @@ private:
 	void build_zone(QString);
 	void load_nif_list(QString);
 	void add_nifs_to_xml(QString);
+	void save_xml(QString);
 	void write_height(QString);
-	void build_height(void);
+	void read_zone_offsets(QString);
+	void calc_total_zone(void);
+	void build_height(QString);
+	void read_tree_conversion(QString);
 	MagickWand *out_img;
 	MagickWand *terrain_img;
 	MagickWand *offset_img;
@@ -38,9 +43,25 @@ private:
 	QDomElement root;
 	QDomElement Vegetation;
 	QDomElement VegetationObject;
-	QList<QString> nif_filenames;
-	QList<QString> nif_name;
-	QMap<int,QDomElement> fixture_element;
+	QMap<int,QString> nif_filenames;
+	QMap<int,QString> nif_name;
+	
+	QMap<QString,QDomElement> fixture_element;
+	QMap<int,int> scalefactor;
+	QMap<int,int> offsetfactor;
+	QMap<int,int> region_offset_x;
+	QMap<int,int> region_offset_y;
+	
+	QMap<QString,QString> nif_group;
+	QMap<QString,QString> nif_cgf;
+	QMap<QString,double> nif_cgf_scale;
+	QMap<QString,bool> nif_snow;
+	
+	
+	
+	int image_size;
+	int low_x;
+	int low_y;
 
 private slots:	    
 	void open_filedialog_daoc(void);
